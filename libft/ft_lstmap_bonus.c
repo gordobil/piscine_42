9dd4fc62a7b549ff71e478d71b00506ce602dd59
gordobil <noahgordobil@gmail.com>
2024-01-29 15:20:24 +0100

    lst done, split w/errors

--- libft/ft_lstmap_bonus.c
+++ libft/ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ngordobi <ngordobi@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 17:02:46 by ngordobi          #+#    #+#             */
-/*   Updated: 2024/01/28 18:26:20 by ngordobi         ###   ########.fr       */
+/*   Updated: 2024/01/29 14:50:48 by ngordobi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,27 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+	t_list	*n_lst;
 	t_list	*l;
 
-	if (!lst)
+	if (!lst || !f || !del)
 		return (0);
-	l = malloc(ft_lstsize(lst) * sizeof(t_list));
+	n_lst = ft_lstnew(f(lst->content));
+	if (!n_lst)
+		return (0);
+	l = n_lst;
+	lst = lst->next;
 	while (lst)
 	{
-		l = f(lst->content);
+		n_lst->next = ft_lstnew(f(lst->content));
+		if (!n_lst->next)
+		{
+			ft_lstclear(&l, del);
+			return (0);
+		}
+		n_lst = n_lst->next;
 		lst = lst->next;
-		l = l->next;
 	}
-	del(lst);
+	n_lst->next = 0;
 	return (l);
 }
