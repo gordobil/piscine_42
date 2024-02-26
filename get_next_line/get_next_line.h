d69e1fa24944bbcb3beba0b0b3c0f1300b7c60ae
gordobil <noahgordobil@gmail.com>
2024-02-26 13:01:03 +0100

    prueba gitignore

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngordobi <ngordobi@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/26 11:36:13 by ngordobi          #+#    #+#             */
+/*   Updated: 2024/02/26 11:41:42 by ngordobi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
