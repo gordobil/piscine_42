5a84a3b186faab306f2343215a774e0f49f53582
gordobil <noahgordobil@gmail.com>
2024-02-26 14:23:43 +0100

    gnl

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,11 +6,24 @@
 /*   By: ngordobi <ngordobi@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 11:36:35 by ngordobi          #+#    #+#             */
-/*   Updated: 2024/02/26 13:41:33 by ngordobi         ###   ########.fr       */
+/*   Updated: 2024/02/26 14:22:54 by ngordobi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*readtxt(int fd, int text)
+{
+	char	*buff;
+	int		i;
+
+	i = 0;
+	while ()
+	{
+		i = read(fd, buff, BUFFER_SIZE);
+
+	}
+}
 
 char	*get_next_line(int fd)
 {
@@ -19,5 +32,17 @@ char	*get_next_line(int fd)
 
 	if (fd < 0)
 		return (NULL);
-	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	readtxt(fd);
+//	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+}
+
+int	main(void)
+{
+	int		fd;
+	char	*gnl;
+
+	fd = open("texto.txt", O_RDONLY);
+	gnl = get_next_line(fd);
+	ft_putstr(gnl);
+	return (0);
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ngordobi <ngordobi@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 11:36:13 by ngordobi          #+#    #+#             */
-/*   Updated: 2024/02/26 11:41:42 by ngordobi         ###   ########.fr       */
+/*   Updated: 2024/02/26 14:01:49 by ngordobi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,12 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <fcntl.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 10
 
 char	*get_next_line(int fd);
+int		ft_putstr(char *str);
 
 #endif
