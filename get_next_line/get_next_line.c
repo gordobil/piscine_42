0c9e4c53bee40291030af8fce60c675d950755df
gordobil <noahgordobil@gmail.com>
2024-02-26 13:43:17 +0100

    gnl start

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ngordobi <ngordobi@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 11:36:35 by ngordobi          #+#    #+#             */
-/*   Updated: 2024/02/26 11:42:28 by ngordobi         ###   ########.fr       */
+/*   Updated: 2024/02/26 13:41:33 by ngordobi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 char	*get_next_line(int fd)
 {
+	static char	*buf;
+	char		line;
 
+	if (fd < 0)
+		return (NULL);
+	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 }
