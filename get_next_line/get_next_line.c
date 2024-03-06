01c9dd7bfb70930aad3018555a0d47f66811aa49
gordobil <noahgordobil@gmail.com>
2024-03-06 15:52:49 +0100

    gnl update

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ngordobi <ngordobi@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 11:36:35 by ngordobi          #+#    #+#             */
-/*   Updated: 2024/03/06 14:25:59 by ngordobi         ###   ########.fr       */
+/*   Updated: 2024/03/06 15:51:23 by ngordobi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ char	*next_line(char *buffer)
 		free(buffer);
 		return (NULL);
 	}
-	line = ft_calloc((ft_strlen(buffer) - i) + 1, sizeof(char));
+	line = ft_calloc((ft_strlen(buffer) - i + 1), sizeof(char));
 	i++;
 	j = 0;
 	while (buffer[i])
@@ -61,6 +61,15 @@ char	*line_stop(char *buffer)
 	return (line);
 }
 
+char	*ft_free(char *buff, char *buffer)
+{
+	char	*temp;
+
+	temp = ft_strjoin(buff, buffer);
+	free(buff);
+	return (temp);
+}
+
 char	*read_file(int fd, char *buff)
 {
 	int		read_bytes;
@@ -69,11 +78,6 @@ char	*read_file(int fd, char *buff)
 	if (!buff)
 		buff = ft_calloc(1, 1);
 	buffer = ft_calloc (BUFFER_SIZE + 1, sizeof(char));
-	/* if (!buffer)
-	{
-		free(buffer);
-		return (NULL);
-	} */
 	read_bytes = 1;
 	while (read_bytes > 0)
 	{
@@ -84,7 +88,7 @@ char	*read_file(int fd, char *buff)
 			return (NULL);
 		}
 		buffer[read_bytes] = '\0';
-		buff = ft_strjoin(buffer, buff);
+		buff = ft_free(buff, buffer);
 		if (ft_strrchr(buffer, '\n'))
 			break ;
 	}
@@ -97,17 +101,17 @@ char	*get_next_line(int fd)
 	static char	*buffer;
 	char		*line;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);
 	buffer = read_file(fd, buffer);
 	if (!buffer)
 		return (NULL);
-	line = ft_strjoin(line, line_stop(buffer));
+	line = line_stop(buffer);
 	buffer = next_line(buffer);
 	return (line);
 }
 
-int	main(void)
+/*int	main(void)
 {
 	int		fd;
 	int		line_count;
@@ -121,16 +125,12 @@ int	main(void)
 	{
 		line = get_next_line(fd);
 		if (line == NULL)
-		{
-//			printf("\nNULL\n");
 			break ;
-		}
 		line_count++;
-		printf("%s/", line);
-//		printf("[%d]: %s\n", line_count, line);
+		printf("[%d]: %s\n", line_count, line);
 		free(line);
 		line = NULL;
 	}
 	close(fd);
 	return (0);
-}
+}*/
